769bf091481fecfbc7047496e800a32edbbc82ef
Shaurya <shauryapratapsingh489@gmail.com>
2026-03-27 22:06:20 +0530

    debugging

--- data_fetchers/books_api.py
+++ data_fetchers/books_api.py
@@ -8,7 +8,7 @@ def get_book_data(book_name):
     headers = {
          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                   "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept": "application/json",
+        "Accept": "application/json"
     }
     api_key=os.getenv("BOOKS_API")
     
