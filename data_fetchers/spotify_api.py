8b3a91f72053da3f69da2bba5052f0627a510e60
Shaurya <shauryapratapsingh489@gmail.com>
2026-03-27 23:16:52 +0530

    finishing up

--- data_fetchers/spotify_api.py
+++ data_fetchers/spotify_api.py
@@ -7,17 +7,21 @@ load_dotenv()
 def get_artist_data(artist_name):
     api_key=os.getenv("LASTFM_API_KEY")
     url=f"http://ws.audioscrobbler.com/2.0/?method=artist.search&artist={artist_name}&api_key={api_key}&format=json"
+    imageurl=f"https://api.deezer.com/search/artist?q={artist_name}"
     response=requests.get(url)
+    imageresponse=requests.get(imageurl)
     if(response.status_code==200):
         data=response.json()
+        imagedata=imageresponse.json()
+        deezer_results=imagedata.get('data', [])
         results=data.get('results',{}).get('artistmatches',{}).get('artist',[])
+        artist_image = deezer_results[0].get('picture_xl')
         if(results):
             artist_info=results[0]
-            images=artist_info.get("image")
-            image_url=images[-1].get("#text") if images else None
+            
             features={
                 "title":artist_info.get("name"),
-                "image":image_url, 
+                "image":artist_image, 
                 "engagement":int(artist_info.get("listeners",0)),
                 "media_type":"music"
             }
